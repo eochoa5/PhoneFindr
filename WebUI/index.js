d0b53f8d5556d616939ca004be28fa5d71aca860
drew2g <nyandrewdesu@live.com>
2017-06-23 02:51:03 -0700

    'new user' socket.on method

--- WebUI/index.js
+++ WebUI/index.js
@@ -3,18 +3,35 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+users = [];
+connections = [];
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname+'/index.html');
 
 });
 
 app.use(express.static(__dirname));
+//drew2g 6/23 ===============================
+//push socket to connection array and console log it
+//===========================================
 
 io.on('connection', function(socket){
-	console.log('a user has connected: ' + socket.id);
+	connections.push(socket);
+	console.log('[C] %s sockets connected. New user %s', connections.length, socket.id);
 
 	socket.on('disconnect', function(){
-		console.log('a user has disconnected: ' + socket.id);
+		console.log('[D] User %s has disconnected', socket.id);
+	});
+
+	socket.on('new user', function(email, callback){
+		if(users.indexOf(email) != -1){
+			callback(false);
+			return;
+		}	
+		callback(true);
+		socket.email = email;
+		users.push(socket.email);
 	});
 
 });
@@ -22,5 +39,4 @@ io.on('connection', function(socket){
 http.listen(process.env.PORT || 3000, function(){
 	console.log('server running');
 });
-
 
