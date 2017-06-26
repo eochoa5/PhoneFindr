b9e5311ad1bb0be45e33651f74dbdc468a94dbdf
eochoa5 <edwinochoa57@gmail.com>
2017-06-25 22:15:48 -0700

    socket io send ring request

--- PhoneFindr/app/src/main/java/com/example/edwin/phonefindr/ActivityLoggedIn.java
+++ PhoneFindr/app/src/main/java/com/example/edwin/phonefindr/ActivityLoggedIn.java
@@ -76,7 +76,7 @@ public class ActivityLoggedIn extends AppCompatActivity {
         */
 
         mPlayer.start();
-        
+
     }
 
 
--- WebUI/index.js
+++ WebUI/index.js
@@ -8,13 +8,47 @@ app.get('/', function(req, res){
 
 });
 
+users = [];
+
 app.use(express.static(__dirname));
 
 io.on('connection', function(socket){
+
 	console.log('a user has connected: ' + socket.id);
 
+
+	socket.on('new user', function(data){
+		socket.email = data;
+		users.push(socket);
+
+		/*
+		console.log("USERS: " );
+
+		for(i=0 ; i<users.length; i++){
+			console.log("email : " + users[i].email + " id: " + users[i].id);
+
+		}
+		*/
+		
+		
+	});
+
+	socket.on('ring', function(data){
+		
+		for(i=0 ; i<users.length; i++){
+			if(users[i].email == data && users[i].id != socket.id){
+
+				io.to(users[i].id).emit('ring request', socket.id);
+			}
+
+		}
+	});
+
 	socket.on('disconnect', function(){
+
 		console.log('a user has disconnected: ' + socket.id);
+
+		users.splice(users.indexOf(socket), 1);
 	});
 
 });
