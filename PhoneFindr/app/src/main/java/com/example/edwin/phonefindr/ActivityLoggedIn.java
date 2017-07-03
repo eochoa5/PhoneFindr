60d096dd8591291951c2eb0fad560f31a955c33a
eochoa5 <edwinochoa57@gmail.com>
2017-07-02 18:13:22 -0700

    rm unnecessary imports

--- PhoneFindr/app/src/main/java/com/example/edwin/phonefindr/ActivityLoggedIn.java
+++ PhoneFindr/app/src/main/java/com/example/edwin/phonefindr/ActivityLoggedIn.java
@@ -3,31 +3,22 @@ package com.example.edwin.phonefindr;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.URISyntaxException;
 
 import com.github.nkzawa.emitter.Emitter;
